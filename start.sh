a64919520a4e9741a6b8b99b3691fae711f22042
Sebastian Svensson <github@tmpfs.xyz>
2019-05-09 14:59:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-cat << _EOF_ > /etc/fusiondirectory/fusiondirectory.conf
-<?xml version="1.0"?>
-<conf>
-  <main default="ldap"
-        logging="TRUE"
-        displayErrors="FALSE"
-        forceSSL="FALSE"
-        templateCompileDirectory="/var/spool/fusiondirectory/"
-        debugLevel="0"
-    >
-
-    <location name="ldap"
-    >
-        <referral URI="${FD_ADMIN_URI}"
-                        adminDn="${FD_ADMIN_DN}"
-                        adminPassword="${FD_ADMIN_PASSWORD}" />
-    </location>
-  </main>
-</conf>
-_EOF_
+#cat << _EOF_ > /etc/fusiondirectory/fusiondirectory.conf
+#<?xml version="1.0"?>
+#<conf>
+#  <main default="ldap"
+#        logging="TRUE"
+#        displayErrors="FALSE"
+#        forceSSL="FALSE"
+#        templateCompileDirectory="/var/spool/fusiondirectory/"
+#        debugLevel="0"
+#   >
+#
+#    <location name="ldap"
+#    >
+#        <referral URI="${FD_ADMIN_URI}"
+#                        adminDn="${FD_ADMIN_DN}"
+#                        adminPassword="${FD_ADMIN_PASSWORD}" />
+#    </location>
+#  </main>
+#</conf>
+#_EOF_
 
 exec /usr/sbin/apache2 -D FOREGROUND
